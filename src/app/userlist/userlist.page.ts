f2ec460b2f4efe22ca2e94d65dc038d8edc82ca4
unknown <maurofassi17@gmail.com>
2022-11-05 09:48:53 -0300

    Consumiendo api

---
+++ src/app/userlist/userlist.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-userlist',
+  templateUrl: './userlist.page.html',
+  styleUrls: ['./userlist.page.scss'],
+})
+export class UserlistPage implements OnInit {
+  datos: any;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('https://rickandmortyapi.com/api/character').subscribe(res=>{
+      this.datos=res;
+      console.log(res);
+    });
+  }
+
+}
